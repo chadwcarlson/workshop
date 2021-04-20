9d6ebcf2f0c48d418bcd16f9be6e21072fbc343e
chadcarlson <chad.carlson@platform.sh>
2021-04-20 13:44:29 -0400

    Clean up for now.,

--- scripts/seed_data.sh
+++ scripts/seed_data.sh
@@ -5,10 +5,11 @@ if [ -z "${PLATFORM_BRANCH}" ]; then
     ln -s data/.env .env
 else
     # Move committed files from temp directory back into mounts.
-    ./scripts/handle_mounts.sh
-
+    # ./scripts/handle_mounts.sh
+    
     # Food advisor seed data to SQLite.
     RELATIONSHIPS=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq '.keys')
+    # Check to make sure seed has not already occurred, AND that PostgreSQL doesn't exist.
     if [ $RELATIONSHIPS = 'null' ] && [ ! -f ~/.seed/sqlite-seeded.txt ]; then
         yarn seed
         echo "SQLite database seeded" > ~/.seed/sqlite-seeded.txt
