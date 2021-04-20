be6ecf8beb2d5e106bd61004d239f26c6dfaa27a
chadcarlson <chad.carlson@platform.sh>
2021-04-20 13:16:28 -0400

    Fresh start check.

---
+++ scripts/seed_data.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -z "${PLATFORM_BRANCH}" ]; then
+    # Allow for seeding by making .env writable temporarily.
+    ln -s data/.env .env
+else
+    # Move committed files from temp directory back into mounts.
+    ./scripts/handle_mounts.sh
+
+    # Food advisor seed data to SQLite.
+    RELATIONSHIPS=$(echo $PLATFORM_RELATIONSHIPS | base64 --decode | jq '.keys')
+    if [ $RELATIONSHIPS = 'null' ] && [ ! -f ~/.seed/sqlite-seeded.txt ]; then
+        yarn seed
+        echo "SQLite database seeded" > ~/.seed/sqlite-seeded.txt
+        mv ~/data/uploads/* ~/public/uploads/
+    fi
+fi
