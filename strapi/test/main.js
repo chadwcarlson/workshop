739fc8bc3ca1014dd787d797b46e55c18aec6f3b
chadcarlson <chad.carlson@platform.sh>
2021-04-20 15:28:00 -0400

    Add test.

---
+++ strapi/test/main.js
@@ -0,0 +1,197 @@
+// let should = require('chai').should();
+// let expect = require('chai').expect;
+let supertest = require('supertest');
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+
+chai.should();
+chai.use(chaiHttp);
+
+let api;
+
+if (process.env.PUBLIC_URL) {
+    api = supertest(process.env.PUBLIC_URL);
+} else {
+    api = supertest("localhost:1337")
+}
+
+// let api = supertest('https://www.master-7rqtwti-hshhhiqeooj36.us-4.platformsh.site');
+
+describe('Verify collection endpoints are accessible', () => {
+
+    describe("Test GET route /categories", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/categories')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/categories/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+
+    describe("Test GET route /histories", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/histories')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(403);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/histories/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(403);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+
+    describe("Test GET route /likes", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/likes')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/likes/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+    
+    describe("Test GET route /restaurants", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/restaurants')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/restaurants/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+
+    describe("Test GET route /reviews", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/reviews')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/reviews/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+
+    describe("Test GET route /universals", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/universals')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('array');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/universals/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(200);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+
+    describe("Test GET route /users", () => {
+
+        it("find", function (done) {
+            this.timeout(5000);
+            api.get('/users')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(403);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+
+        it("findone", function (done) {
+            this.timeout(5000);
+            api.get('/users/1')
+            .set('Accept', 'application/json')
+            .end((err, response) => {
+                response.should.have.status(403);
+                response.body.should.be.a('object');
+            done();
+            })
+        })
+    })
+})
