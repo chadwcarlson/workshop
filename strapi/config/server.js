5a5d88aa25bfcc859f94d9cc5e86dd7fa9fc1810
chadcarlson <chad.carlson@platform.sh>
2021-04-21 21:37:55 -0400

    Add email support.

--- strapi/config/server.js
+++ strapi/config/server.js
@@ -1,8 +1,23 @@
+const forgotPasswordTemplate = {
+    subject: "Strapi: Reset your password",
+    html: `<p>We heard that you lost your password. Sorry about that!</p>
+        <p>But don’t worry! You can use the following link to reset your password:</p>
+        <p><a href="<%= url %>">Reset password</a></p>
+        <p>Thanks!</p>`,
+    text: `We heard that you lost your password. Sorry about that!
+    But don’t worry! You can use the following link to reset your password:
+    <%= url %>
+    Thanks!`
+}
+
 module.exports = ({ env }) => ({
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET'),
     },
+    forgotPassword: {
+      emailTemplate: forgotPasswordTemplate
+    }
   },
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
